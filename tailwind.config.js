7bbcfcd4cccef9e735bcf3a249f5c6ade868127f
Stefan Turner <stefanturner@MacBook-Air-von-Stefan.local>
2022-12-09 06:21:28 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'rubik': ['"Rubik"', 'sans-serif'],
+      },
+      colors: {
+        'primary': 'hsl(246, 80%, 60%)',
+        'work-red': 'hsl(15, 100%, 70%)',
+        'soft-blue': 'hsl(195, 74%, 62%)',
+        'study-red': 'hsl(348, 100%, 68%)',
+        'lime-green': 'hsl(145, 58%, 55%)',
+        'social-violet': 'hsl(264, 64%, 52%)',
+        'soft-orange': 'hsl(43, 84%, 65%)',
+        'vd-blue': 'hsl(226, 43%, 10%)',
+        'dark-blue': 'hsl(235, 46%, 20%)',
+        'des-blue': 'hsl(235, 45%, 61%)',
+        'pale-blue': 'hsl(236, 100%, 87%)',
+      },
+    },
+  },
+  plugins: [],
+}
