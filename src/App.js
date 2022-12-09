fbe0d0a9a7f16dcbdb1d66e54690c99acedf97aa
Stefan Turner <turner.stefan281@gmail.com>
2022-12-09 10:28:48 +0100

    add usercard with mobile style

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { User } from './components/User';
 function App() {
   return (
     <div className="App">
-      <main className=" w-full h-screen bg-vd-blue font-rubik text-white">
-        <div className="grid">
+      <main className=" w-full h-screen flex justify-center items-center bg-vd-blue font-rubik text-white">
+        <div className="grid w-11/12">
           <User />
         </div>
       </main>
--- src/components/User.js
+++ src/components/User.js
@@ -2,13 +2,20 @@ import profileImg from '../assets/images/image-jeremy.png';
 
 export const User = () => {
   return (
-    <div className="">
-      <img src={profileImg} alt="" />
-      <h1>
-        <span>Report for</span>
-        <br />
-        Jeremy Robson
-      </h1>
+    <div className=" bg-dark-blue rounded-xl">
+      <div className='bg-primary p-6 rounded-xl flex items-center'>
+        <img src={profileImg} className=" w-16 border-2 border-white rounded-full" alt="" />
+        <h1 className=' font-light leading-7 text-2xl pl-4'>
+          <span className='text-sm opacity-70'>Report for</span>
+          <br />
+          Jeremy Robson
+        </h1>        
+      </div>
+      <div className=' p-6 flex justify-between'>
+        <button className='btn'>Daily</button>
+        <button className='btn'>Weekly</button>
+        <button className='btn'>Monthly</button>
+      </div>
     </div>
   );
 };
