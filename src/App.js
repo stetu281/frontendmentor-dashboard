ca0ba9f8f6a7a2e97d996fc0ac36288f1cfcb3be
Stefan Turner <stefanturner@MacBook-Air-von-Stefan.local>
2022-12-10 04:22:10 +0100

    update styling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { ActivityCard } from './components/ActivityCard';
 import data from './assets/data/data.json';
 
 function App() {
+  const handleClick = () => {
+    console.log('click');
+  };
 
   return (
     <div className="App">
       <main className=" w-full min-h-screen py-20 flex justify-center items-center bg-vd-blue font-rubik text-white">
         <div className="grid gap-y-6 w-11/12 max-w-xl lg:max-w-[1120px] lg:grid-cols-4 lg:grid-rows-2 lg:gap-7">
-          <User />
-          {
-            data.map((item, index) => (
-              <ActivityCard activity={data[index]} key={index} id={index} />
-            ))
-           }
+          <User handleClick={handleClick} />
+          {data.map((item, index) => (
+            <ActivityCard activity={data[index]} key={index} id={index} />
+          ))}
         </div>
       </main>
     </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,27 @@
 import profileImg from '../assets/images/image-jeremy.png';
 
-export const User = () => {
+export const User = (props) => {
   return (
     <div className=" bg-dark-blue rounded-xl lg:row-span-2">
-      <div className='bg-primary p-6 rounded-xl flex items-center lg:flex-col lg:items-start lg:p-8 lg:pb-14'>
+      <div className="bg-primary p-6 rounded-xl flex items-center lg:flex-col lg:items-start lg:p-8 lg:pb-14">
         <img src={profileImg} className=" w-16 border-2 border-white rounded-full" alt="" />
-        <h1 className=' font-light leading-7 text-2xl pl-4 lg:pl-0 lg:mt-5 lg:text-4xl'>
-          <span className='text-sm opacity-70'>Report for</span>
+        <h1 className=" font-light leading-7 text-2xl pl-4 lg:pl-0 lg:mt-5 lg:text-4xl">
+          <span className="text-sm opacity-70">Report for</span>
           <br />
           Jeremy Robson
-        </h1>        
+        </h1>
       </div>
-      <div className=' p-6 flex justify-between lg:flex-col lg:items-start lg:p-8'>
-        <button className='btn active'>Daily</button>
-        <button className='btn'>Weekly</button>
-        <button className='btn'>Monthly</button>
+      <div className=" p-6 flex justify-between lg:flex-col lg:items-start lg:p-8">
+        <button
+          onClick={(e) => {
+            props.handleClick(console.log(e));
+          }}
+          className="btn active"
+        >
+          Daily
+        </button>
+        <button className="btn">Weekly</button>
+        <button className="btn">Monthly</button>
       </div>
     </div>
   );
